1dae9bd413dce6f17e2f39c473278ee490f7a2ac
Espen Holtebu <Espen.Holtebu@gmail.com>
2024-01-08 00:42:32 +0100

    Hello World!

---
+++ src/constants.js
@@ -0,0 +1,15 @@
+const prod = {
+  url: {
+    BASE_URL: "http://brettspill.localhost:8008/",
+    AUTH_URL: "http://brettspill.localhost:8008/auth",
+  },
+};
+
+const dev = {
+  url: {
+    BASE_URL: "http://brettspill.holtebu.eu/",
+    AUTH_URL: "http://brettspill.holtebu.eu/auth/",
+  },
+};
+
+export const config = process.env.NODE_ENV === "development" ? dev : prod;
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js", // Entry point of your application
+  output: {
+    filename: "bundle.js", // Output bundle file name
+    path: path.resolve(__dirname, "dist"), // Output directory
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    }, // Serve files from this directory
+    port: 3000, // Port for the development server
+    open: true, // Open the default web browser when the server starts
+  },
+};
