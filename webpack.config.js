755baa593b18761f0177f5badb1d96febcb8a8d9
Espen Holtebu <Espen.Holtebu@gmail.com>
2024-02-09 21:27:44 +0100

    Optimaliserer react ved dev

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,10 @@ module.exports = (env, argv) => {
           use: {
             loader: "babel-loader",
             options: {
-              presets: ["@babel/preset-env", "@babel/preset-react"],
+              presets: [
+                "@babel/preset-env",
+                ["@babel/preset-react", { runtime: "automatic" }],
+              ],
             },
           },
         },
