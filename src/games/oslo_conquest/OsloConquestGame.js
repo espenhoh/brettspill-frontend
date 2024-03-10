c61b4617a3c1362a6f92bdbfa529f59cc7948a6b
Espen Holtebu <Espen.Holtebu@gmail.com>
2024-03-11 00:24:16 +0100

    Delbydeler i Stovner

--- src/games/oslo_conquest/OsloConquestGame.js
+++ src/games/oslo_conquest/OsloConquestGame.js
@@ -11,32 +11,39 @@ class OsloConquest extends Phaser.Scene {
   }
 
   create() {
-    var scene = this;
-    var graphics = this.add.graphics();
-
-    // Definer punktene for polygonet
-
-    // Definer punkter for polygoner i et JavaScript-objekt
+    let scene = this;
+    let graphics = this.add.graphics();
 
     // Tegn polygoner fra objektet
-    Object.entries(gameMap).forEach(function ([name, data]) {
-      var points = data.points;
-      var color = data.color;
-      var sted = data.name;
-
-      graphics.fillStyle(color, 1);
-      graphics.fillPoints(
-        points.map(function (point) {
-          return new Phaser.Geom.Point(point[0], point[1]);
-        }),
-        true
-      );
+    for (const bydel of gameMap.bydeler) {
+      for (const delbydel of bydel.delbydeler) {
+        graphics.fillStyle(bydel.color, 1);
+        graphics.fillPoints(
+          delbydel.points.map(function (point) {
+            return new Phaser.Geom.Point(point[0], point[1]);
+          }),
+          true
+        );
+        console.log(delbydel.name.text);
+        // Plasser tekst i polygon
+        let delbydelNavn = scene.add.text(
+          delbydel.name.position[0],
+          delbydel.name.position[1],
+          delbydel.name.text,
+          {
+            fontFamily: "Arial",
+            fontSize: 10,
+            color: "#000000",
+          }
+        );
+        delbydelNavn.setOrigin(0.5); // Sentrer tekst
+      }
 
       // Plasser tekst i polygon
-      var textObject = scene.add.text(
-        sted.position[0],
-        sted.position[1],
-        sted.text,
+      let textObject = scene.add.text(
+        bydel.name.position[0],
+        bydel.name.position[1],
+        bydel.name.text,
         {
           fontFamily: "Arial",
           fontSize: 16,
@@ -44,7 +51,7 @@ class OsloConquest extends Phaser.Scene {
         }
       );
       textObject.setOrigin(0.5); // Sentrer tekst
-    });
+    }
 
     // Gjør polygonet synlig
     graphics.setVisible(true);
