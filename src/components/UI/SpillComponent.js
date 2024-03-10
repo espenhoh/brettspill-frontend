d3714d3a2688a3199d7b2c09b59266124babe1e7
Espen Holtebu <Espen.Holtebu@gmail.com>
2024-03-10 02:30:34 +0100

    hhh

--- src/components/UI/SpillComponent.js
+++ src/components/UI/SpillComponent.js
@@ -2,14 +2,17 @@ import React, { useEffect } from "react";
 
 import styles from "./SpillComponent.module.css";
 
+import bydeler from "./Bydeler.png";
+
 const SpillComponent = ({ config }) => {
   useEffect(() => {
     const game = new Phaser.Game(config);
-
     return () => game.destroy(true);
   }, []);
 
-  return <div id="game_container"></div>;
+  return (
+    <div className={styles.game_container_holder} id="game_container"></div>
+  );
 };
 
 export default SpillComponent;
