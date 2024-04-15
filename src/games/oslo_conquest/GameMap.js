edfb51336d0064fd9c41af4f24e80f3e5d1fae95
Espen Holtebu <Espen.Holtebu@gmail.com>
2024-04-15 22:48:40 +0200

    Abstraherer til egent BoardPiece objekt

--- src/games/oslo_conquest/GameMap.js
+++ src/games/oslo_conquest/GameMap.js
@@ -3,38 +3,51 @@ import Phaser from "phaser";
 export class GameMap {
   constructor(gameMap) {
     this.map = gameMap;
-    this.polygons = [];
+    this.boardPieces = [];
 
     for (const tile of this.map.tiles) {
       for (const subtile of tile.subtiles) {
-        subtile.polygon = new Phaser.Geom.Polygon(subtile.points);
-        this.polygons.push(subtile.polygon);
+        let boardPiece = new BoardPiece(subtile.points, tile.color);
+        this.boardPieces.push(boardPiece);
       }
     }
   }
 
   drawMap(graphics) {
-    for (const polygon of this.polygons) {
-      this.#drawPolygon(graphics, polygon);
+    for (const boardPiece of this.boardPieces) {
+      this.#drawPolygon(graphics, boardPiece);
     }
-    console.log(this.polygons);
+    console.log(this.boardPieces);
   }
 
-  #drawPolygon(graphics, polygon) {
+  #drawPolygon(graphics, boardPiece) {
     // Sett linjefarge og -tykkelse
     graphics.lineStyle(2, 0x000000); // Tykkelse 2, farge grønn
-    graphics.fillStyle(0xeb4034, 1); // Rød farge
+    graphics.fillStyle(boardPiece.color, 1); // Rød farge
     // Tegn polygonlinje
     graphics.beginPath();
-    graphics.moveTo(polygon.points[0].x, polygon.points[0].y);
-    for (var i = 1; i < polygon.points.length; i++) {
-      graphics.lineTo(polygon.points[i].x, polygon.points[i].y);
+    graphics.moveTo(
+      boardPiece.polygon.points[0].x,
+      boardPiece.polygon.points[0].y
+    );
+    for (var i = 1; i < boardPiece.polygon.points.length; i++) {
+      graphics.lineTo(
+        boardPiece.polygon.points[i].x,
+        boardPiece.polygon.points[i].y
+      );
     }
     graphics.closePath();
     graphics.fillPath();
     graphics.strokePath();
-    console.log(polygon);
+    console.log(boardPiece);
 
     //graphics.fillStyle(bydel.color, 1);
+  }
+}
+
+class BoardPiece {
+  constructor(points, color) {
+    this.polygon = new Phaser.Geom.Polygon(points);
+    this.color = color;
   }
 }
