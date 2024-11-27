0c7acbb77831cb744bca25e7a61c2bb97f1e007c
Espen Holtebu <Espen.Holtebu@gmail.com>
2024-11-27 23:51:00 +0100

    Tror deletes er fikset

--- src/util/deletes.ts
+++ src/util/deletes.ts
@@ -1,17 +1,18 @@
 import axios, { AxiosResponse } from "axios";
 import { Response } from "webpack-dev-server";
+import SpillData from "../pages/CreateGame";
 
 function erOK(response: AxiosResponse) {
   return response.status >= 200 && response.status < 300;
 }
 
 export async function slettSpill(spillId: number) {
-  return await slett(`/lobby/spill/${spillId}/`);
+  return await slett<typeof SpillData>(`/lobby/spill/${spillId}/`);
 }
 
-async function slett(url: string) {
+async function slett<T>(url: string) {
   try {
-    const response = await axios.delete<>(url);
+    const response = await axios.delete<T>(url);
 
     if (!erOK(response)) {
       throw { message: "Failed to delete", status: response.status };
