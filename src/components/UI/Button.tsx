d298e188142d66ce49bd5ced8f95ec6fce65b911
Espen Holtebu <Espen.Holtebu@gmail.com>
2024-11-27 23:48:36 +0100

    Fikset Button

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import { FC } from "react";
 
-import classes from './Button.module.css';
+import classes from "./Button.module.css";
 
-const Button = (props) => {
+type PropsType = {
+  type?: "button" | "submit" | "reset";
+  className: string;
+  disabled: boolean;
+  onClick?: (values: any) => void;
+  children?: React.ReactNode
+};
+
+const Button: FC<PropsType> = (props) => {
   return (
     <button
-      type={props.type || 'button'}
+      type={props.type || "button"}
       className={`${classes.button} ${props.className}`}
       onClick={props.onClick}
       disabled={props.disabled}
     >
-      {props.children}
+      {props. children}
     </button>
   );
 };
