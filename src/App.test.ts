0bcd51d10829cc892a277be83a0a4f8c9c6d8672
Espen Holtebu <Espen.Holtebu@gmail.com>
2024-11-30 00:46:43 +0100

    Tester

---
+++ src/App.test.ts
@@ -0,0 +1,6 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("Dummytest", () => {
+  expect(2 + 2).toBe(4);
+});
--- src/util/gets.ts
+++ src/util/gets.ts
@@ -1,33 +1,76 @@
-import axios, { AxiosResponse } from "axios";
+import axios, {
+  AxiosRequestConfig,
+  AxiosResponse,
+  RawAxiosRequestHeaders,
+} from "axios";
+import config from "../constants";
 import { json } from "react-router-dom";
+import { z } from "zod";
 
-function erOK(response: AxiosResponse) {
-  return response.status >= 200 && response.status < 300;
-}
+const client = axios.create({
+  baseURL: config.url.BASE_URL,
+});
+
+const getConfig: AxiosRequestConfig = {
+  headers: {
+    Accept: "application/json",
+  } as RawAxiosRequestHeaders,
+};
 
 export async function getSpillListe() {
   return await get(`/lobby/spill/`);
 }
 
-export async function getSpill(id: number) {
-  return await get(`/lobby/spill/${id}/`);
+interface Spiller {
+  username: string;
+}
+
+interface Spill {
+  id: number;
+  spill_type_navn: string;
+  spill_navn: string;
+  spill_type: string;
+  opprettet_tid: string;
+  start_tid: string | null;
+  slutt_tid: string | null;
+  spillere: Spiller[]; // Bytt `any[]` med en spesifikk type om spillere har en bestemt struktur
 }
 
 export async function getSpillTyper() {
   return await get("/lobby/spill/get_alle_spill_typer/");
 }
 
-async function get(url: string) {
-  const response = await axios.get(url, {
-    headers: { Accept: "application/json" },
-  });
+export async function getSpill(id: number): Promise<Spill> {
+  const data = await fetchValidatedData<Spill>(`/lobby/spill/${id}/`,
 
-  if (!erOK(response)) {
+  );
+
+  const response = await client.get<Spill>(`/lobby/spill/${id}/`, getConfig);
+  if (validResponse(response)) {
+  }
+
+  return response;
+}
+
+async function fetchValidatedData<T>(
+  url: string,
+  validateFn?: (response: AxiosResponse) => void
+): Promise<T> {
+  const response = await client.get<T>(url, getConfig);
+
+  validResponse(response);
+
+  return response.data;
+}
+
+function validResponse(response: AxiosResponse): void {
+  if (!(response.status >= 200 && response.status < 300)) {
     throw json({ message: "Failed to fetch." }, { status: response.status });
   }
+
   const responseType = response.headers["content-type"];
-  if (responseType.includes("html")) {
+  if (responseType && responseType.includes("html")) {
+    const url = response.config?.url || "ukjent endepunkt";
     throw json({ message: `Fant ikke endepunkt: ${url}` }, { status: 404 });
   }
-  return response;
 }
