bf69e5e417468d258aacf4b49638a910dedba3dd
Espen Holtebu <Espen.Holtebu@gmail.com>
2024-12-08 00:11:02 +0100

    Fikset tester

--- src/util/gets.test.ts
+++ src/util/gets.test.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import { getSpill } from "./gets";
 import { get } from "https";
 
@@ -8,6 +8,10 @@ const mockData = {
 };
 
 describe("Mocked axios tests", () => {
+  beforeEach(() => {
+    jest.resetModules();
+  });
+
   it("should fetch spill data", async () => {
     // Testdata for mock
     jest.spyOn(axios, "get").mockResolvedValueOnce({
@@ -21,28 +25,63 @@ describe("Mocked axios tests", () => {
     const spill = await getSpill(1);
 
     expect(spill).toEqual(mockData);
-    expect(axios.get).toHaveBeenCalledWith(
-      "/lobby/spill/1/",
-      expect.anything()
-    );
+    expect(axios.get).toHaveBeenCalled();
   });
 
-  /*
   it("should fail", async () => {
     // Testdata for mock
-    (axios.get as jest.Mock).mockRejectedValue({
+    jest.spyOn(axios, "get").mockRejectedValueOnce({
       data: mockData,
       status: 400,
-      statusText: "Ok",
+      statusText: "Bad Request",
+    });
+
+    await expect(getSpill(-1)).rejects.toEqual({
+      data: mockData,
+      status: 400,
+      statusText: "Bad Request",
+    });
+  });
+
+  test("throws error for invalid status code", async () => {
+    jest.spyOn(axios, "get").mockResolvedValueOnce({
+      data: {},
+      status: 404,
+      statusText: "Not Found",
       headers: {},
       config: {},
     });
-    debugger;
-    return getSpill(-1).then((spill) => {
-      expect(spill).rejects.toMatch("Failed to fetch.");
-    });
+
+    try {
+      await getSpill(1);
+    } catch (error: any) {
+      expect(error).toEqual({
+        message: "Failed to fetch.",
+        status: 404,
+      });
+    }
   });
-  */
+
+  test("HTML content type means that endpoint doesnt exist", async () => {
+    jest.spyOn(axios, "get").mockResolvedValueOnce({
+      data: {},
+      status: 200,
+      statusText: "OK",
+      headers: {
+        "content-type": "text/html", // Simulerer HTML content type
+      },
+      config: { url: "https://example.com" },
+    });
+
+    try {
+      await getSpill(1);
+    } catch (error: any) {
+      expect(error).toEqual({
+        message: "Fant ikke endepunkt: https://example.com",
+        status: 404,
+      });
+    }
+  });
 });
 
 describe("Integration tests", () => {
