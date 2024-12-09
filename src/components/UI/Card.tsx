511fd5c9b7f4baca68b25763fc7c2324b7d1d24f
Espen Holtebu <Espen.Holtebu@gmail.com>
2024-12-09 20:04:10 +0100

    Skrevet om Card til ts, men jeg tror ikke Card brukes

--- src/components/UI/Card.tsx
+++ src/components/UI/Card.tsx
@@ -1,10 +1,9 @@
-import './Card.css'
+import { FC } from "react";
+import "./Card.css";
 
-const Card = (props) => {
-    const classes = 'card ' + props.className;
-    return (
-        <div className={classes}>{props.children}</div>
-    );
-}
+const Card: FC<{ className: string; children: React.ReactNode }> = (props) => {
+  const classes = "card " + props.className;
+  return <div className={classes}>{props.children}</div>;
+};
 
 export default Card;
