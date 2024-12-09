ac989d1ebfd1552c12928e5d417781f31292cff0
Espen Holtebu <Espen.Holtebu@gmail.com>
2024-12-09 19:45:53 +0100

    Koverterer Logout til Typescript

--- src/pages/Feilside.tsx
+++ src/pages/Feilside.tsx
@@ -1,22 +1,25 @@
 import { useSelector } from "react-redux";
-import { useRouteError } from "react-router-dom";
+import { useRouteError, isRouteErrorResponse } from "react-router-dom";
 import Logout from "../components/Content/Logout";
 import Sidenav from "../components/Sidenav/Sidenav";
+import { extractErrorMessage } from "../util/errorHandling";
+
+import type { RootState } from "../store/index";
 
 const Feilside = () => {
-    const auth = useSelector((state) => state.auth);
-    const error = useRouteError();
+  const auth = useSelector((state: RootState) => state.auth);
+  const errorMessage = extractErrorMessage(useRouteError());
 
-    return (
-        <>
-            {auth.logoutVisible ? <Logout /> : null}
-            <Sidenav/>
-            <main>
-                <h1>Det har skjedd en feil!</h1>
-                <p>{error.data.message}</p>
-            </main>
-        </>
-    )
-}
+  return (
+    <>
+      {auth.logoutVisible ? <Logout /> : null}
+      <Sidenav />
+      <main>
+        <h1>Det har skjedd en feil!</h1>
+        <p>{errorMessage}</p>
+      </main>
+    </>
+  );
+};
 
 export default Feilside;
