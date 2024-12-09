4457bb279a679afa274d127e8a6b63c1521baaaa
Espen Holtebu <Espen.Holtebu@gmail.com>
2024-12-09 19:03:02 +0100

    Konvertert Logout til typescript

--- src/components/Content/Logout.tsx
+++ src/components/Content/Logout.tsx
@@ -1,22 +1,26 @@
-import React from "react";
+import React, { FC } from "react";
 
 import { useDispatch } from "react-redux";
-import { authActions} from "../../store/authSlice";
+import { authActions } from "../../store/authSlice";
 
 import Button from "../UI/Button";
 import Modal from "../UI/Modal";
 
-const Logout = props => {
-    const dispatch = useDispatch();
+const Logout: FC = () => {
+  const dispatch = useDispatch();
 
-    const hideModal = (event) => {
-        dispatch(authActions.loggedOut());
-    };
+  const hideModal = () => {
+    dispatch(authActions.loggedOut());
+  };
 
-    return <Modal>
-        <h1>Du er nå logget ut!</h1>
-        <Button disabled={false} onClick={hideModal}>OK</Button>
+  return (
+    <Modal>
+      <h1>Du er nå logget ut!</h1>
+      <Button className="logout-button" disabled={false} onClick={hideModal}>
+        OK
+      </Button>
     </Modal>
+  );
 };
 
 export default Logout;
