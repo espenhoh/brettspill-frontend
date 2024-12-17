994f347cf98dbb9cc9539de80aa05c063d8ab54b
Espen Holtebu <Espen.Holtebu@gmail.com>
2024-12-17 22:52:29 +0100

    Modal til typescript

--- src/components/UI/Modal.tsx
+++ src/components/UI/Modal.tsx
@@ -1,13 +1,13 @@
-import React, { Fragment } from "react";
+import React, { FC, Fragment, PropsWithChildren } from "react";
 import ReactDOM from "react-dom";
 
 import classes from "./Modal.module.css";
 
-const Backdrop = (props) => {
+const Backdrop: FC = () => {
   return <div className={classes.backdrop} />;
 };
 
-const ModalOverlay = (props) => {
+const ModalOverlay: FC<PropsWithChildren> = (props) => {
   return (
     <div className={classes.modal}>
       <div className={classes.content}>{props.children}</div>
@@ -15,12 +15,19 @@ const ModalOverlay = (props) => {
   );
 };
 
-const Modal = (props) => {
+const Modal: FC<PropsWithChildren> = (props) => {
   const portalElement = document.getElementById("overlays");
+  if (!portalElement) {
+    return <div>Ingen overlay funnet!!!</div>;
+  }
+
   return (
     <Fragment>
       {ReactDOM.createPortal(<Backdrop />, portalElement)}
-      {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
     </Fragment>
   );
 };
