594b98b6a47478743af81fa0b296799a60bfaad1
Espen Holtebu <Espen.Holtebu@gmail.com>
2024-12-19 23:08:12 +0100

    Ryddet opp og skrevet posts til ts

--- src/util/posts.ts
+++ src/util/posts.ts
@@ -1,6 +1,8 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
 import { SpillData } from "../pages/CreateGame";
 
+import type { Spill, SpillError } from "../types/api";
+
 export function erOK(response: AxiosResponse) {
   return response.status >= 200 && response.status < 300;
 }
@@ -9,40 +11,61 @@ export function erOK(response: AxiosResponse) {
 //  return await post("/lobby/login/", payload);
 //}
 
-export async function postNyttSpill(payload: SpillData) {
-  return await post("/lobby/spill/", payload);
+export async function postNyttSpill(
+  payload: SpillData
+): Promise<Spill | SpillError> {
+  return await post<Spill, SpillError>("/lobby/spill/", payload);
 }
 
-async function post(url: string, payload: SpillData | any) {
+async function post<T, TError>(
+  url: string,
+  payload: SpillData | any
+): Promise<T | TError> {
   try {
     const response = await axios.post(url, payload, {
       headers: { "Content-Type": "application/json" },
     });
-    /*
-    if(response.code === 400) {
-      return response;
-    }
 
     if (!erOK(response)) {
       throw { message: "Failed to post", status: response.status };
-    }*/
+    }
 
     return response.data;
   } catch (e) {
-    if (axios.isAxiosError(e)) {
-      // Access to config, request, and response
-      const error = e as AxiosError;
-      if (error.response) {
-        return { errors: error.response.data };
-      } else if (error.request) {
-        // The request was made but no response was received
-        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-        // http.ClientRequest in node.js
-        console.log(error.request);
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.log("Error", error.message);
-      }
+    return errorHandler<TError>(e);
+  }
+}
+
+function errorHandler<TError>(e: any): TError {
+  if (axios.isAxiosError(e)) {
+    // Access to config, request, and response
+    const error = e as AxiosError;
+    if (error.response) {
+      return { errors: error.response.data } as TError;
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      throw {
+        message: `Ingen respons fra server ved URL ${error.config?.url}`,
+        status: 503,
+      };
+    } else {
+      // En annen feil i oppsettet
+      throw {
+        message: `Feil ved oppsett av request: ${error.message}`,
+        status: 400,
+      };
     }
+  } else if (e instanceof Error) {
+    throw {
+      message: `En annen feil oppstod: ${e.message}`,
+      status: 500,
+    };
+  } else {
+    throw {
+      message: `Jeg gir opp: ${e}`,
+      status: 500,
+    };
   }
 }
