9ae5f7f1d2841720a7e92a756b288aace079385c
Espen Holtebu <Espen.Holtebu@gmail.com>
2025-01-05 22:41:49 +0100

    Håndtere css moduler

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,23 @@
-import type { Config } from "jest";
+import type { JestConfigWithTsJest } from "ts-jest";
 
-const config: Config = {
+const config: JestConfigWithTsJest = {
   verbose: true,
   setupFilesAfterEnv: ["./jest.setup.ts"],
   transform: {
-    "^.+.tsx?$": ["ts-jest", {}],
+    "^.+.tsx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
   },
+  extensionsToTreatAsEsm: [".ts", ".tsx"],
   testTimeout: 50000,
+  moduleNameMapper: {
+    // Håndter CSS-moduler
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
 };
 
 export default config;
